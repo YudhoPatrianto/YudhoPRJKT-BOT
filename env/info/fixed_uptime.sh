6b6fb0b914cf88fdb30094f9fd59e9219e8d61c2
YudhoPatrianto <kydh01123@gmail.com>
2023-11-13 12:48:00 +0700

    env: info: silence prompt convert using dos2unix
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- env/info/fixed_uptime.sh
+++ env/info/fixed_uptime.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install dos2unix -y > /dev/null 2>&1
 
-dos2unix $(ls $PWD/env/info/uptime_info.sh) 
+dos2unix $(ls $PWD/env/info/uptime_info.sh) > /dev/null 2>&1
 
 bash $(ls $PWD/env/info/uptime_info.sh)
